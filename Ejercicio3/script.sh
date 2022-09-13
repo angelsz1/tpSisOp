5fbedc6095fe4dbcc858ea6ce8a18bb8f73c9b74
angelsz <eliasszust@gmail.com>
2022-09-12 21:02:14 -0300

    Ejercicio3Corregido

--- Ejercicio3/script.sh
+++ Ejercicio3/script.sh
@@ -47,92 +47,114 @@ compilar(){
     ls | grep -r '' | cut -d ":" -f 2- > $pathActual"/"$2
     cd $pathActual
 }
+if [[ $7 = "" ]];then
 
-#si la ruta es vacia, uso Desktop
+    if [[ $1 != "-c" ]]; then
+        echo "Error: debe utilizar -c para especificar el directorio a monitorizar"
+        echo "Para mas informacion, ejecute $0 -h"
+        exit 1
+    elif [[ $2 = "" || ! -d $2 ]]; then
+        echo "Error: no se especifico un directorio valido"
+        echo "Para mas informacion, ejecute $0 -h"
+        exit 1
+    fi
 
-ruta=$2
+    if [[ $3 != "-a" ]]; then
+        echo "Error: debe utilizar -a para listar las acciones a realizar"
+        echo "Para mas informacion, ejecute $0 -h"
+        exit 1
+    elif [[ $4 = "" ]]; then
+        echo "Error: No se especifico ninguna accion a realizar"
+        echo "Para mas informacion, ejecute $0 -h"
+        exit 1
+    fi
 
-if [[ $2 = "" ]]; then
-    ruta="/home/`whoami`/Desktop"
+    #manejo del -a
+    acciones=`echo $4 | tr "," " "`
+
+    #chequeo restricciones de acciones
+    for i in $acciones; do
+        if [[ $i != "listar" && $i != "peso" && $i != "compilar" && $i != "publicar" ]]; then
+            echo "La accion $i no es valida"
+            exit 1
+        fi
+    done
+
+    if [[ $acciones =~ "publicar" ]];then
+        if [[ $5 != "-s" ]]; then
+            echo "Si la accion \"publicar\" es seleccionada, la accion \"compilar\" debe ser seleccionada, y se debe proveer un directorio con el comando -s para publicar el archivo compilado"
+            exit 1
+        else
+            if [[ $6 = "" ]]; then
+                echo "La accion publicar requiere un directorio de publicacion"
+                exit 1
+            else
+                if [[ ! -r $6 ]]; then
+                    echo "No tienes permisos de lectura sobre la ruta $6"
+                    exit 1
+                fi
+            fi
+            if [[ ! -d $6 ]]; then
+                mkdir $6
+            fi
+            if [[ ! $acciones =~ "compilar" ]]; then
+                echo "La accion publicar requiere la accion compilar"
+                exit 1
+            fi
+        fi
+
+        
+    elif [[ $5 = "-s" ]]; then
+        echo "La accion publicar no fue seleccionada"
+        exit 1
+    fi
 fi
 
 
 #creo el demonio
 > nohup.out
 if [[ $7 = "" ]]; then
-    nohup ./script.sh $1 $ruta $3 $4 $5 $6 "1" &
+    nohup ./script.sh $1 $2 $3 $4 $5 $6 "1" &
     exit 0
 fi
 echo $0
 
-
-#codigo del script
+#manejo del -a
+acciones=`echo $4 | tr "," " "`
 
 #chequeo por bin
 bintest=`ls | grep "bin" | wc -w`
-if [[ $bintest -ne 1 ]]; then
-    mkdir bin
-fi
-if [[ ! -f "bin/concat.bin" ]]; then
-    touch bin/concat.bin
+if [[ $acciones =~ "compilar" ]]; then
+    if [[ $bintest -ne 1 ]]; then
+        mkdir bin
+    fi
+    if [[ ! -f "bin/concat.con" ]]; then
+        touch bin/concat.con
+    fi
+
+    compilar $2 bin/concat.con
+    if [[ $acciones =~ "publicar" ]]; then
+        cp bin/concat.con $6
+    fi
+
 fi
 
 
-rutaScript=`pwd`
-cd $ruta
-ruta=`pwd`
-echo "Monitorizando $ruta"
-cd $rutaScript
+echo "Monitorizando $2"
+
 
 
 #chequeo permisos de lectura sobre la ruta
-if [[ ! -r $ruta ]]; then 
-    echo "No tienes permisos de lectura sobre la ruta $ruta"
+if [[ ! -r $2 ]]; then 
+    echo "No tienes permisos de lectura sobre la ruta $2"
     exit 1
-fi
-
-#manejo del -a
-acciones=`echo $4 | tr "," " "`
-echo $acciones
-
-#chequeo restricciones de acciones
-for i in $acciones; do
-    if [[ $i != "listar" && $i != "peso" && $i != "compilar" && $i != "publicar" ]]; then
-        echo "La accion $i no es valida"
-        exit 1
-    fi
-done
-
-if [[ $acciones =~ "publicar" ]];then
-    if [[ $6 = "1" ]]; then
-        echo "La accion publicar requiere un directorio de publicacion"
-        exit 1
-    else
-        if [[ ! -r $6 ]]; then
-            echo "No tienes permisos de lectura sobre la ruta $6"
-            exit 1
-        fi
-    fi
-    if [[ ! -d $6 ]]; then
-        mkdir $6
-    fi
-    if [[ ! $acciones =~ "compilar" ]]; then
-        echo "La accion publicar requiere la accion compilar"
-        exit 1
-    fi
-    if [[ $5 = "1" ]]; then
-        echo "La accion publicar requiere un path de publicacion"
-        exit 1
-    fi
-
 fi
 
 
 #vacio el log
 > monitor_log
 
-inotifywait -q -m -r -e create,modify,delete,moved_to $ruta | while read DIRECTORIO EVENTO ARCHIVO; do
-    #echo $EVENTO
+inotifywait -q -m -r -e create,modify,delete,moved_to $2 | while read DIRECTORIO EVENTO ARCHIVO; do
     case $EVENTO in
         MODIFY*)
             if [[ $acciones =~ "listar" ]]; then
@@ -142,11 +164,11 @@ inotifywait -q -m -r -e create,modify,delete,moved_to $ruta | while read DIRECTO
             fulDir="$DIRECTORIO$ARCHIVO"
                 echo "Peso = `du --apparent-size -h $fulDir`" | awk '{print $1,$2,$3}' | uniq >> monitor_log
             fi
-            if [[ $acciones =~ "compilar" ]]; then #concatenar los archivos en $ruta y guardar en bin/concat.bin
-                compilar $ruta "bin/concat.bin"
+            if [[ $acciones =~ "compilar" ]]; then #concatenar los archivos en $2 y guardar en bin/concat.con
+                compilar $2 "bin/concat.con"
             fi
-            if [[ $acciones =~ "publicar" ]]; then #publicar el archivo bin/concat.bin en $6
-                cp bin/concat.bin $6
+            if [[ $acciones =~ "publicar" ]]; then #publicar el archivo bin/concat.con en $6
+                cp bin/concat.con $6
             fi
                 
             ;;
@@ -158,11 +180,11 @@ inotifywait -q -m -r -e create,modify,delete,moved_to $ruta | while read DIRECTO
                 fulDir="$DIRECTORIO$ARCHIVO"
                 echo "Peso = `du --apparent-size -h $fulDir`" | awk '{print $1,$2,$3}' | uniq>> monitor_log
             fi
-            if [[ $acciones =~ "compilar" ]]; then #concatenar los archivos en $ruta y guardar en bin/concat.bin
-                compilar $ruta "bin/concat.bin"
+            if [[ $acciones =~ "compilar" ]]; then #concatenar los archivos en $2 y guardar en bin/concat.con
+                compilar $2 "bin/concat.con"
             fi
-            if [[ $acciones =~ "publicar" ]]; then #publicar el archivo bin/concat.bin en $6
-                cp bin/concat.bin $6
+            if [[ $acciones =~ "publicar" ]]; then #publicar el archivo bin/concat.con en $6
+                cp bin/concat.con $6
             fi
             ;;
         DELETE*)
@@ -170,11 +192,11 @@ inotifywait -q -m -r -e create,modify,delete,moved_to $ruta | while read DIRECTO
                 archivo_borrado "$DIRECTORIO" "$ARCHIVO"
             fi
             #no muestro peso porque no se puede calcular
-            if [[ $acciones =~ "compilar" ]]; then #concatenar los archivos en $ruta y guardar en bin/concat.bin
-                compilar $ruta "bin/concat.bin"
+            if [[ $acciones =~ "compilar" ]]; then #concatenar los archivos en $2 y guardar en bin/concat.con
+                compilar $2 "bin/concat.con"
             fi
-            if [[ $acciones =~ "publicar" ]]; then #publicar el archivo bin/concat.bin en $6
-                cp bin/concat.bin $6
+            if [[ $acciones =~ "publicar" ]]; then #publicar el archivo bin/concat.con en $6
+                cp bin/concat.con $6
             fi
             ;;
         #RENAMED
@@ -186,11 +208,11 @@ inotifywait -q -m -r -e create,modify,delete,moved_to $ruta | while read DIRECTO
                 fulDir="$DIRECTORIO$ARCHIVO"
                 echo "Peso = `du --apparent-size -h $fulDir`" | awk '{print $1,$2,$3}' | uniq >> monitor_log
             fi
-            if [[ $acciones =~ "compilar" ]]; then #concatenar los archivos en $ruta y guardar en bin/concat.bin
-                compilar $ruta "bin/concat.bin"
+            if [[ $acciones =~ "compilar" ]]; then #concatenar los archivos en $2 y guardar en bin/concat.con
+                compilar $2 "bin/concat.con"
             fi
-            if [[ $acciones =~ "publicar" ]]; then #publicar el archivo bin/concat.bin en $6
-                cp bin/concat.bin $6
+            if [[ $acciones =~ "publicar" ]]; then #publicar el archivo bin/concat.con en $6
+                cp bin/concat.con $6
             fi
             ;;
     esac
