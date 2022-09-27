53ecddda54ad3aa7d7b68bed86296462bdd699cf
agosmottu <112671714+agostinaMottura@users.noreply.github.com>
2022-09-27 12:43:38 -0300

    Update ejercicio-6.ps1

--- TP2/Ejercicio6/ejercicio-6.ps1
+++ TP2/Ejercicio6/ejercicio-6.ps1
@@ -1,3 +1,11 @@
+#APL N. 2
+#SCRIPT 6
+#INTEGRANTES:
+#Carballo, Facundo Nicolas (DNI: 42774931)
+#Garcia Burgio, Matias Nicolas (DNI: 42649117)
+#Mottura, Agostina Micaela (DNI: 41898101)
+#Povoli Olivera, Victor (DNI: 43103780)
+#Szust, Ángel Elías (DNI: 43098495)
 <#
 .DESCRIPTION
 Este script emula el comportamiento de una papelera de reciclaje.
