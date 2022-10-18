5804e95f749f515daa06d47a84324926d677fa9c
Matias Garcia <matingb@yahoo.com.ar>
2022-10-18 08:41:58 -0300

    create a deamon process

--- TP3/Ejercicio4/main.c
+++ TP3/Ejercicio4/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    pid_t process_id = 0;
+
+    process_id = fork();
+    if(process_id < 0) {
+        printf("fork failed!\n");
+        exit(1);
+    }
+    if (process_id > 0) {
+        printf("process_id of child process %d \n", process_id);
+        exit(0);
+    }
+
+    FILE* archivo = fopen("archivo.txt", "rb");
+    while (1)
+    {
+        sleep(1);
+        fprintf(archivo, "Logging info...\n");
+    }
+    fclose(archivo);
+    return (0);
 }
