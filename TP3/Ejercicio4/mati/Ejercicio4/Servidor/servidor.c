2b8fb2178d3fc03cda82469c01604faa6dc61c0b
Matias Garcia <43641235+matingb@users.noreply.github.com>
2022-10-20 18:58:56 -0300

    create server

--- TP3/Ejercicio4/mati/Ejercicio4/Servidor/servidor.c
+++ TP3/Ejercicio4/mati/Ejercicio4/Servidor/servidor.c
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <unistd.h>
+
+void crearServidor();
 
 int main()
 {
+
+    crearServidor();
+
     char* clave = "../refugio";
     key_t key = ftok(clave, 4);
 
@@ -15,4 +21,20 @@ int main()
     *areaCompartida = 3;
 
     return 0;
+}
+
+void crearServidor() {
+    pid_t process_id = 0;
+
+    process_id = fork();
+
+    if(process_id < 0) {
+        printf("fork failed!\n");
+        exit(1);
+    }
+
+    if (process_id > 0) {
+        printf("process_id of child process %d \n", process_id);
+        exit(0);
+    }
 }
